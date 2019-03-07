ec6ed1aa7ff8a635fceae252a0d6abadf4b699fc
Monica Gomez Avila <mgavila@beta11.aulas.gsyc.urjc.es>
2019-03-07 10:48:43 +0100

    Bola animadada y coge teclado

--- pong-01.js
+++ pong-01.js
@@ -8,6 +8,17 @@ function main()
 
   var ctx = canvas.getContext("2d");
 
+  //sacar coasas del navegador o ver cosas que maneje el navegador
+  window.onkeydown = (event) => {
+    //para usar solo tu la tecla que no la utilice el navegadop
+    console.log(event.key);
+    event.preventDefault();
+    if (event.key == "a") {
+      console.log("una a loco");
+    }
+  }
+
+/*
   ctx.fillStyle = "white";
   ctx.fillRect(50,50,10,40);
   ctx.fillRect(550,310,10,40);
@@ -29,4 +40,76 @@ function main()
   ctx.fillStyle = 'white';
   ctx.fill()
 
+  */
+
+  var bola = {
+
+    //Posicion inicial
+    x_ini : 50,
+    y_ini :50,
+
+    x : 0,
+    y : 0,
+
+    //De esto depende el angulo de rebote
+    vel_x : 3,
+    vel_y : 1,
+
+    ctx : null,
+
+    //Bola cuadrada
+    width : 5,
+    height : 5,
+
+    reset :function() {
+      this.x = this.x_ini;
+      this.y = this.y_ini;
+    },
+
+    init : function(ctx) {
+      this.reset();
+      this.ctx = ctx;
+    },
+
+    draw : function() {
+      this.ctx.fillStyle = "white";
+      this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    },
+
+    update : function() {
+      console.log("update")
+      this.x = this.x + this.vel_x;
+      this.y = this.y +this.vel_y;
+    }
+  }
+
+  //se inicializa bola
+  bola.init(ctx);
+  bola.draw();
+
+  //ANIMANDO
+  var timer = null;
+
+  var sacar = document.getElementById("sacar");
+
+  sacar.onclick = () => {
+    //Sólo activa timer si no ha sido ya activado
+    if (!timer) {
+      timer = setInterval(()=>{
+        //--Actualizar bola, saber nueva posicion en bola UPDATE
+        bola.update();
+        ctx.clearRect(0,0,canvas.width, canvas.height)
+        bola.draw();
+        //--Condicion de terminar, mcuando salga del canvas para
+        if (bola.x > canvas.width){
+          clearInterval(timer);
+          timer = null;
+          //reiniciar bola posicion inicializa
+          bola.reset();
+          bola.draw();
+        }
+
+      }, 20)
+    }
+  }
 }
